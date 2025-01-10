b670d6287dcf5a4a04d1e8153e109951f8bd2a28
Alexjr2 <52109110+Alexjr2@users.noreply.github.com>
2025-01-10 18:26:52 +0800

    Update frida-python.sh

--- frida-python.sh
+++ frida-python.sh
@@ -5,6 +5,25 @@ if ! command -v termux-setup-storage; then
   exit 1
 fi
 
+case "$(uname -m)" in
+    aarch64)
+        arch="arm64"
+        ;;
+    armv7l | armv8l)
+        arch="arm"
+        ;;
+    x86_64)
+        arch="x86_64"
+        ;;
+    x86)
+        arch="x86"
+        ;;
+    *)
+        echo "System architecture not recognized: $(uname -m)"
+        exit 1
+        ;;
+esac
+
 cd $TMPDIR
 
 apt update && pkg upgrade -y
@@ -13,11 +32,11 @@ pkg i -y python git && pip install -U setuptools
 
 FRIDA_VERSION=$(curl --silent "https://api.github.com/repos/Alexjr2/Frida_Termux_Installation/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
 
-DEVKIT_URL="https://github.com/Alexjr2/Frida_Termux_Installation/releases/download/$FRIDA_VERSION/frida-core-devkit-android-arm64.tar.xz"
+DEVKIT_URL="https://github.com/Alexjr2/Frida_Termux_Installation/releases/download/$FRIDA_VERSION/frida-core-devkit-android-$arch.tar.xz"
 
-curl -L -o frida-core-devkit-android-arm64.tar.xz "$DEVKIT_URL"
+curl -L -o frida-core-devkit-android-$arch.tar.xz "$DEVKIT_URL"
 
-mkdir -p $PWD/devkit && tar -xJvf frida-core-devkit-android-arm64.tar.xz -C $PWD/devkit
+mkdir -p $PWD/devkit && tar -xJvf frida-core-devkit-android-$arch.tar.xz -C $PWD/devkit
 
 git clone https://github.com/AbhiTheModder/frida-python frida-python-android
 
